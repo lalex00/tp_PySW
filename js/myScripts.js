877e1c697bfbbafcb26f30c105cfe14033833c8b
Ramos Alex <lalex00@live.com.ar>
2026-05-02 21:16:44 -0300

    cards added

--- js/myScripts.js
+++ js/myScripts.js
@@ -32,3 +32,14 @@ $(document).ready(function(){
         }, 300);
     });
 });
+/* counter */
+$('.Count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 1000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
